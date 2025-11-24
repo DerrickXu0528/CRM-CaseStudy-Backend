8f62aa5d86190f524cd2989fb63002082488e7d4
Derrick Xu <xu.ruohan@outlook.com>
2025-11-24 18:05:24 -0500

    Update CORS for producion

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ app = FastAPI(title="CRM Lead Management API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "http://127.0.0.1:3000"],
+                   "http://127.0.0.1:3000",
+                   "https://crm-case-study-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
